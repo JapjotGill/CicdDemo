c9c7d5c2d44c7ba5cac3110da57c3a55d1c881fa
Admin <admin@Admins-MacBook-Pro.local>
2025-08-24 21:47:49 -0400

    Initial commit for Cicd Demo

---
+++ src/test/java/com/aws/cicd/CicdDemo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.cicd.CicdDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
